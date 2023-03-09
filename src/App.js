be8faa9883be215a4d042147db5bdfacbae3c39c
maria-sirb <sirbmariafelicia@gmail.com>
2023-03-09 17:09:14 +0200

    add basic components navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import React, {Component} from "react";
+import { Education } from "./components/Education";
+import { Experience } from "./components/Experience";
+import { Personal } from "./components/Personal";
+import { componentsMap } from "./componentsMap";
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  
+  constructor(){
+    super();
+    this.state = {
+      currentStep : "Personal"
+    };
+
+    this.changeStep = this.changeStep.bind(this);
+  }
+
+  
+
+  changeStep(e) {
+    
+    this.setState({currentStep: e.target.id});
+
+  }
+
+  render() { 
+
+    let Step = componentsMap[this.state.currentStep];
+    return(
+      <div className="App">
+        <div className = "Steps">
+          <div onClick={this.changeStep} id = "Personal">Personal</div>
+          <div onClick={this.changeStep} id = "Education">Education</div>
+          <div onClick={this.changeStep} id = "Experience">Experience</div>
+        </div>
+      <Step title = {this.state.currentStep}/> 
+      </div>
+    );
+  }
 }
 
 export default App;
