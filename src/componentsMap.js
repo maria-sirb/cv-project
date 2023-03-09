bc24c9f518ff633b69f4be894ad09bd551e91589
maria-sirb <sirbmariafelicia@gmail.com>
2023-03-09 17:08:37 +0200

    add components name mapping

---
+++ src/componentsMap.js
@@ -0,0 +1,10 @@
+import { Education } from "./components/Education";
+import { Experience } from "./components/Experience";
+import { Personal } from "./components/Personal";
+
+export let componentsMap =  {
+
+    "Personal" : Personal,
+    "Experience" : Experience,
+    "Education" : Education
+}
