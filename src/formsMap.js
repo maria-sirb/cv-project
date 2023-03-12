a0a401e068db2ffb580fc3be21aa1d6632ef0e78
maria-sirb <sirbmariafelicia@gmail.com>
2023-03-12 14:20:39 +0200

    add forms map

---
+++ src/formsMap.js
@@ -0,0 +1,7 @@
+import { EducationForm } from "./components/EducationForm";
+import { WorkForm } from "./components/WorkForm";
+
+export let formsMap = {
+    "Education" : EducationForm,
+    "Work" : WorkForm
+}
